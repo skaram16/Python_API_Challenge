7b81e1826bb4c47ec9e26153fb5c3fd35b14ead4
skaram16 <sabrinakaram@Sabrinas-MacBook-Pro.local>
2021-03-18 23:04:22 -0400

    adding files

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "8bce802f60bc75d38652850bc2d5b16d"
+weather_key = "8bce802f60bc75d38652850bc2d5b16d"
+
+
+# Google API Key
+google_key = "AIzaSyDSUWhWFopGIZOzAKaU6KfLQ-UfmTf9yx0"
